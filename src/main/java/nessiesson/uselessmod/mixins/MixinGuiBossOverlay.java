40d985e3afca1221a1f27a82182d64f9cbf3aa35
nessie <nessie@hotmail.dk>
2018-08-28 18:13:11 +0200

    add more epic jokes.

--- src/main/java/nessiesson/uselessmod/mixins/MixinGuiBossOverlay.java
+++ src/main/java/nessiesson/uselessmod/mixins/MixinGuiBossOverlay.java
@@ -10,10 +10,14 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(GuiBossOverlay.class)
 public abstract class MixinGuiBossOverlay {
-	private static final String[] op = {"rid is hella gae", "CARPETMOD IS NOT VANILLA IDIOT",
+	private static final String[] op = {
+			"rid is hella gae", "CARPETMOD IS NOT VANILLA IDIOT",
 			"mango has best hot streams ofc", "duping is for lazy idiots", "man are u actually retarded?",
-			"mind ur own business", "more vanilla than spigot", "I'll build a statue of you on the SciCraft server!",
-			"btw u heard of the new scicrap guy moddit?", "only lazy jerks will read this message.", "moronzz best!"};
+			"mind you r own business", "more vanilla than spigot", "I'll build a statue of you on the SciCraft server!",
+			"btw u heard of the new scicrap guy moddit?", "only lazy jerks will read this message.", "moronzz best!",
+			"JC Denton. 23 years old. No residence. No ancestors. No employer. No \u2013",
+			"I am a prototype for a much larger system.", "kk ur floor is burning"
+	};
 
 	@Redirect(method = "renderBossHealth", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/BossInfoClient;getName()Lnet/minecraft/util/text/ITextComponent;"))
 	private ITextComponent hotterStream(BossInfoClient bossInfoClient) {
