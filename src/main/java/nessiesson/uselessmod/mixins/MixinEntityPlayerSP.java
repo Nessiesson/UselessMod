267bcd3380ad24d38c2b5fdd8bdda5e388c9a978
nessie <nessie@hotmail.dk>
2018-09-08 10:46:47 +0200

    Actually commit the classes from 9c223feaac4be8ed987928ad75d7d2f02fe76ccc. >_>

--- src/main/java/nessiesson/uselessmod/mixins/MixinEntityPlayerSP.java
+++ src/main/java/nessiesson/uselessmod/mixins/MixinEntityPlayerSP.java
@@ -1,16 +1,29 @@
 package nessiesson.uselessmod.mixins;
 
+import com.mojang.authlib.GameProfile;
+import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EntityPlayerSP.class)
-public abstract class MixinEntityPlayerSP {
+public abstract class MixinEntityPlayerSP extends AbstractClientPlayer {
+	public MixinEntityPlayerSP(World worldIn, GameProfile playerProfile) {
+		super(worldIn, playerProfile);
+	}
+
 	@Inject(method = "canUseCommand", at = @At("HEAD"), cancellable = true)
 	private void overrideCommandPermissions(int permLevel, String commandName, CallbackInfoReturnable<Boolean> cir) {
 		cir.setReturnValue(true);
+	}
 
+	@Inject(method = "onLivingUpdate", at = @At(value = "INVOKE", ordinal = 0, shift = At.Shift.AFTER,
+			target = "Lnet/minecraft/client/network/NetHandlerPlayClient;sendPacket(Lnet/minecraft/network/Packet;)V"))
+	private void fixElytraDeployment(CallbackInfo ci) {
+		this.setFlag(7, true);
 	}
 }
