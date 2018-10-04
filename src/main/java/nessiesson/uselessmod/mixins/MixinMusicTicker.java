09c06ad4601ad9b0c154820ff5d7883ebfae985e
nessie <nessie@hotmail.dk>
2018-10-04 13:58:08 +0200

    For now just use an @Overwrite in the musicticker. not like this mod is meant to be used with others anyway.

--- src/main/java/nessiesson/uselessmod/mixins/MixinMusicTicker.java
+++ src/main/java/nessiesson/uselessmod/mixins/MixinMusicTicker.java
@@ -1,19 +1,45 @@
 package nessiesson.uselessmod.mixins;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.audio.ISound;
 import net.minecraft.client.audio.MusicTicker;
+import net.minecraft.client.audio.PositionedSoundRecord;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MusicTicker.class)
 public abstract class MixinMusicTicker {
 	@Shadow
+	@Final
+	private Minecraft mc;
+	@Shadow
+	private ISound currentMusic;
+	@Shadow
 	private int timeUntilNextMusic;
 
-	@Inject(method = "playMusic", at = @At("RETURN"))
-	private void continuousMusic(MusicTicker.MusicType requestedMusicType, CallbackInfo ci) {
-		this.timeUntilNextMusic = 100;
+	//TODO: stop overwrite.
+	@Overwrite
+	public void update() {
+		MusicTicker.MusicType musicType = this.mc.getAmbientMusicType();
+
+		if (this.currentMusic != null) {
+			if (!musicType.getMusicLocation().getSoundName().equals(this.currentMusic.getSoundLocation())) {
+				this.mc.getSoundHandler().stopSound(this.currentMusic);
+				this.timeUntilNextMusic = 100;
+			}
+
+			if (!this.mc.getSoundHandler().isSoundPlaying(this.currentMusic)) {
+				this.currentMusic = null;
+				this.timeUntilNextMusic = 100;
+			}
+		}
+
+		if (this.currentMusic == null && this.timeUntilNextMusic-- <= 0) {
+			this.currentMusic = PositionedSoundRecord.getMusicRecord(musicType.getMusicLocation());
+			this.mc.getSoundHandler().playSound(this.currentMusic);
+			this.timeUntilNextMusic = 100;
+		}
 	}
 }
