00729324599b5f8ec27f47079ec0672301de6085
nessie <nessie@hotmail.dk>
2018-10-04 13:53:54 +0200

    Remove gr8 elytra fix since vanilla seems more reliable. xd

--- src/main/java/nessiesson/uselessmod/mixins/MixinEntityPlayerSP.java
+++ src/main/java/nessiesson/uselessmod/mixins/MixinEntityPlayerSP.java
@@ -7,7 +7,6 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EntityPlayerSP.class)
@@ -19,10 +18,5 @@ public abstract class MixinEntityPlayerSP extends AbstractClientPlayer {
 	@Inject(method = "canUseCommand", at = @At("HEAD"), cancellable = true)
 	private void overrideCommandPermissions(int permLevel, String commandName, CallbackInfoReturnable<Boolean> cir) {
 		cir.setReturnValue(true);
-	}
-
-	@Inject(method = "onLivingUpdate", at = @At(value = "FIELD", target = "Lnet/minecraft/network/play/client/CPacketEntityAction$Action;START_FALL_FLYING:Lnet/minecraft/network/play/client/CPacketEntityAction$Action;"))
-	private void fixElytraDeployment(CallbackInfo ci) {
-		this.setFlag(7, true);
 	}
 }
