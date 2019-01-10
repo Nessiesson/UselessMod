3c81217f522ddabe882f27904b1923a0df962176
nessie <nessie@hotmail.dk>
2019-01-10 05:50:29 +0100

    Add fix for https://bugs.mojang.com/browse/MC-31222.

---
+++ src/main/java/nessiesson/uselessmod/mixins/MixinGuiScreen.java
@@ -0,0 +1,30 @@
+package nessiesson.uselessmod.mixins;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@SuppressWarnings("RedundantCast")
+@Mixin(GuiScreen.class)
+public abstract class MixinGuiScreen {
+	@Shadow
+	protected Minecraft mc;
+
+	@Inject(method = "handleInput", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiScreen;handleMouseInput()V"), cancellable = true)
+	private void mc31222Mouse(CallbackInfo ci) {
+		if ((GuiScreen) (Object) this != this.mc.currentScreen) {
+			ci.cancel();
+		}
+	}
+
+	@Inject(method = "handleInput", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiScreen;handleKeyboardInput()V"), cancellable = true)
+	private void mc31222Keyboard(CallbackInfo ci) {
+		if ((GuiScreen) (Object) this != this.mc.currentScreen) {
+			ci.cancel();
+		}
+	}
+}
