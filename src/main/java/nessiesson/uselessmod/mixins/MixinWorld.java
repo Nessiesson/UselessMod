0005f1e756532e3b3d97aaec60ce1b1b450ec868
nessie <nessie@hotmail.dk>
2019-01-24 15:24:08 +0100

    Add fix for https://bugs.mojang.com/browse/MC-117087.

---
+++ src/main/java/nessiesson/uselessmod/mixins/MixinWorld.java
@@ -0,0 +1,37 @@
+package nessiesson.uselessmod.mixins;
+
+import net.minecraft.profiler.Profiler;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.Iterator;
+
+@Mixin(World.class)
+public class MixinWorld {
+	@Shadow
+	@Final
+	public Profiler profiler;
+
+	private TileEntity te;
+
+	@Inject(method = "updateEntities", at = @At(value = "INVOKE", target = "Ljava/lang/Class;getSimpleName()Ljava/lang/String;", remap = false), locals = LocalCapture.CAPTURE_FAILSOFT)
+	private void keepACopy(CallbackInfo ci, Iterator iterator, TileEntity tileentity) {
+		this.te = tileentity;
+	}
+
+	@Redirect(method = "updateEntities", at = @At(value = "INVOKE", target = "Ljava/lang/Class;getSimpleName()Ljava/lang/String;", remap = false))
+	private String mc117087(Class aClass) {
+		if (this.profiler.profilingEnabled) {
+			return te.getClass().getSimpleName();
+		}
+		return "";
+	}
+}
