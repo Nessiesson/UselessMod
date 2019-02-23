10ee864ea9694748a701a025ac2fca12fb3ee920
nessie <nessie@hotmail.dk>
2019-02-23 16:56:11 +0100

    Make it so spectator menu always opens instead of useless options menu.

---
+++ src/main/java/nessiesson/uselessmod/mixins/MixinSpectatorMenu.java
@@ -0,0 +1,21 @@
+package nessiesson.uselessmod.mixins;
+
+import net.minecraft.client.gui.spectator.ISpectatorMenuObject;
+import net.minecraft.client.gui.spectator.ISpectatorMenuRecipient;
+import net.minecraft.client.gui.spectator.SpectatorMenu;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(SpectatorMenu.class)
+public abstract class MixinSpectatorMenu {
+	@Shadow
+	public abstract ISpectatorMenuObject getItem(int index);
+
+	@Inject(method = "<init>", at = @At("RETURN"))
+	private void onlyShowPlayers(ISpectatorMenuRecipient menu, CallbackInfo ci) {
+		this.getItem(0).selectItem((SpectatorMenu) (Object) this);
+	}
+}
