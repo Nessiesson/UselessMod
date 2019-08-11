dd968ef4666dbe219acf84b7069245c969a58263
nessie <nessie@hotmail.dk>
2019-08-11 23:42:34 +0200

    Sort enchantments by level and then id, and hide death animations.

---
+++ src/main/java/nessiesson/uselessmod/mixins/MixinRenderManager.java
@@ -0,0 +1,23 @@
+package nessiesson.uselessmod.mixins;
+
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(RenderManager.class)
+public abstract class MixinRenderManager {
+	@Inject(method = "renderEntityStatic", at = @At("HEAD"), cancellable = true)
+	private void hideDeathAnimation(Entity entity, float f, boolean z, CallbackInfo ci) {
+		if (!(entity instanceof EntityLivingBase)) {
+			return;
+		}
+		final EntityLivingBase mob = (EntityLivingBase) entity;
+		if (mob.deathTime > 0) {
+			ci.cancel();
+		}
+	}
+}
