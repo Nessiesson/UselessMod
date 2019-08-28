90e0f74ac59f72206c282083f992b7a211a7f361
nessie <nessie@hotmail.dk>
2019-08-28 19:46:14 +0200

    Port to 1.12.2 and remove some useless features.

--- src/main/java/nessiesson/uselessmod/mixins/MixinNetHandlerPlayClient.java
+++ src/main/java/nessiesson/uselessmod/mixins/MixinNetHandlerPlayClient.java
@@ -6,7 +6,6 @@ import net.minecraft.network.play.server.SPacketTimeUpdate;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(NetHandlerPlayClient.class)
@@ -20,11 +19,5 @@ public abstract class MixinNetHandlerPlayClient {
 		if (dt > 0L) {
 			LiteModUselessMod.mspt = Math.max(50.0, dt * 5e-8);
 		}
-	}
-
-	@Redirect(method = "handleTimeUpdate", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/play/server/SPacketTimeUpdate;getWorldTime()J"))
-	private long alwaysDay(SPacketTimeUpdate packet) {
-		final long time = packet.getWorldTime();
-		return time >= 0 ? -(time - time % 24000L + 6000L) : time;
 	}
 }
--- src/main/java/nessiesson/uselessmod/mixins/MixinWorld.java
+++ src/main/java/nessiesson/uselessmod/mixins/MixinWorld.java
@@ -1,6 +1,5 @@
 package nessiesson.uselessmod.mixins;
 
-import net.minecraft.profiler.Profiler;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
@@ -8,20 +7,14 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
 @Mixin(World.class)
 public abstract class MixinWorld {
-	@Shadow
-	@Final
-	public Profiler profiler;
 	@Shadow
 	@Final
 	public List<TileEntity> tickableTileEntities;
@@ -32,19 +25,6 @@ public abstract class MixinWorld {
 	@Final
 	private List<TileEntity> tileEntitiesToBeRemoved;
 	private TileEntity te;
-
-	@Inject(method = "updateEntities", at = @At(value = "INVOKE", target = "Ljava/lang/Class;getSimpleName()Ljava/lang/String;", remap = false), locals = LocalCapture.CAPTURE_FAILSOFT)
-	private void keepACopy(CallbackInfo ci, Iterator iterator, TileEntity tileentity) {
-		this.te = tileentity;
-	}
-
-	@Redirect(method = "updateEntities", at = @At(value = "INVOKE", target = "Ljava/lang/Class;getSimpleName()Ljava/lang/String;", remap = false))
-	private String mc117087(Class aClass) {
-		if (this.profiler.profilingEnabled) {
-			return te.getClass().getSimpleName();
-		}
-		return "";
-	}
 
 	@Inject(method = "updateEntities", at = @At(value = "FIELD", target = "Lnet/minecraft/world/World;tileEntitiesToBeRemoved:Ljava/util/List;", ordinal = 0))
 	private void tileEntityRemovalFix(CallbackInfo ci) {
