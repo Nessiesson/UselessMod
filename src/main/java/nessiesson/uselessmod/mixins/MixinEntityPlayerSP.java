291eb9bfdd83ee7a3b1718e82ea999f04eb66806
nessie <nessie@hotmail.dk>
2019-10-07 13:09:26 +0200

    Remove elytra stuff since Forge fixes it anyway.

--- src/main/java/nessiesson/uselessmod/mixins/MixinEntityPlayerSP.java
+++ src/main/java/nessiesson/uselessmod/mixins/MixinEntityPlayerSP.java
@@ -1,28 +1,15 @@
 package nessiesson.uselessmod.mixins;
 
-import com.mojang.authlib.GameProfile;
-import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.entity.EntityPlayerSP;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EntityPlayerSP.class)
-public abstract class MixinEntityPlayerSP extends AbstractClientPlayer {
-	public MixinEntityPlayerSP(World worldIn, GameProfile playerProfile) {
-		super(worldIn, playerProfile);
-	}
-
+public abstract class MixinEntityPlayerSP {
 	@Inject(method = "canUseCommand", at = @At("HEAD"), cancellable = true)
 	private void overrideCommandPermissions(int permLevel, String commandName, CallbackInfoReturnable<Boolean> cir) {
 		cir.setReturnValue(true);
-	}
-
-	@Inject(method = "isOpenBlockSpace", at = @At("HEAD"), cancellable = true)
-	private void adjustIsOpenBlockSpace(BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
-		cir.setReturnValue(!this.world.getBlockState(pos).isNormalCube());
 	}
 }
