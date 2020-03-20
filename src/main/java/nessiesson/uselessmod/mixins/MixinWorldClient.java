1dafb95d13491cfe9d714b6bab82e31a8bb78b40
nessie <nessie@hotmail.dk>
2020-03-20 13:05:08 +0100

    Let spectators and creative people see BE-less b36.

--- src/main/java/nessiesson/uselessmod/mixins/MixinWorldClient.java
+++ src/main/java/nessiesson/uselessmod/mixins/MixinWorldClient.java
@@ -1,16 +1,28 @@
 package nessiesson.uselessmod.mixins;
 
 import nessiesson.uselessmod.Configuration;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.WorldClient;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityFireworkRocket;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.profiler.Profiler;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldProvider;
 import net.minecraft.world.storage.ISaveHandler;
 import net.minecraft.world.storage.WorldInfo;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.Random;
 
 @Mixin(WorldClient.class)
 public abstract class MixinWorldClient extends World {
@@ -28,5 +40,14 @@ public abstract class MixinWorldClient extends World {
 	@Override
 	public float getRainStrength(float delta) {
 		return Configuration.showRain ? this.prevRainingStrength + (this.rainingStrength - this.prevRainingStrength) * delta : 0F;
+	}
+
+	@Inject(method = "showBarrierParticles", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;randomDisplayTick(Lnet/minecraft/block/state/IBlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Ljava/util/Random;)V", shift = At.Shift.AFTER), cancellable = true, locals = LocalCapture.CAPTURE_FAILHARD)
+	private void showB36(int x, int y, int z, int offset, Random random, boolean holdingBarrier, BlockPos.MutableBlockPos pos, CallbackInfo ci, int i, int j, int k, IBlockState state) {
+		final Block block = state.getBlock();
+		final EntityPlayer player = Minecraft.getMinecraft().player;
+		if (holdingBarrier && block == Blocks.BARRIER || block == Blocks.PISTON_EXTENSION && (player.isSpectator() || player.isCreative()) && this.getTileEntity(pos) == null) {
+			this.spawnParticle(EnumParticleTypes.BARRIER, (float) i + 0.5F, (float) j + 0.5F, (float) k + 0.5F, 0D, 0D, 0D);
+		}
 	}
 }
