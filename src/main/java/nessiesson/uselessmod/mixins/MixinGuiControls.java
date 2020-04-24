840ffe9c28ef65e193e42f71b03efd87967924f8
nessie <nessie@hotmail.dk>
2020-04-24 17:27:13 +0200

    Only allow the reset controls button to work when shift is held down.

---
+++ src/main/java/nessiesson/uselessmod/mixins/MixinGuiControls.java
@@ -0,0 +1,19 @@
+package nessiesson.uselessmod.mixins;
+
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiControls;
+import net.minecraft.client.gui.GuiScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GuiControls.class)
+public abstract class MixinGuiControls {
+	@Inject(method = "actionPerformed", at = @At("HEAD"), cancellable = true)
+	private void resetKeybindsWhenHoldingShiftOnly(GuiButton button, CallbackInfo ci) {
+		if (button.id == 201 && !GuiScreen.isShiftKeyDown()) {
+			ci.cancel();
+		}
+	}
+}
--- src/main/java/nessiesson/uselessmod/mixins/MixinTileEntityPiston.java
+++ src/main/java/nessiesson/uselessmod/mixins/MixinTileEntityPiston.java
@@ -7,9 +7,12 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(TileEntityPiston.class)
 public abstract class MixinTileEntityPiston extends TileEntity {
+	@Unique
+	private static final float USELESSMOD_MATH_NEXT_DOWN_OF_ONE = Math.nextDown(1F);
 	@Shadow
 	private float progress;
 
@@ -21,7 +24,7 @@ public abstract class MixinTileEntityPiston extends TileEntity {
 	@Overwrite
 	public float getProgress(float partialTicks) {
 		if (this.tileEntityInvalid && Math.abs(this.progress - 1F) < 1E-5F) {
-			return 1F;
+			return USELESSMOD_MATH_NEXT_DOWN_OF_ONE;
 		}
 
 		return Math.min(1F, (2F * this.progress + partialTicks) / 3F);
