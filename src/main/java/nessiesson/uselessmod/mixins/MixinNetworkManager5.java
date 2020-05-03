a0e2b03c73bc28b241396d209befd3607a465380
nessie <nessie@hotmail.dk>
2020-05-03 23:46:00 +0200

    Get rid of ugly DOT instead of $ in some mixin class names. Instead put nothing.

--- src/main/java/nessiesson/uselessmod/mixins/MixinNetworkManager5.java
+++ src/main/java/nessiesson/uselessmod/mixins/MixinNetworkManager5.java
@@ -5,7 +5,7 @@ import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(targets = "net/minecraft/network/NetworkManager$5")
-public abstract class MixinNetworkManagerDOL5 {
+public abstract class MixinNetworkManager5 {
 	@ModifyConstant(method = "initChannel", constant = @Constant(intValue = 30), remap = false)
 	private static int noTimeout(int timeoutSeconds) {
 		return 0;
